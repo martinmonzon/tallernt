9e8fc19eba57a1eafde9267de0278a4e9ff00cc4
martinmonzon <marmon17@hotmail.com>
2014-08-04 11:37:30 -0300

    Repositorio Iniciado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TallerNT::Application.config.secret_key_base = '545593ae2468a40875bc16972165939233f466a785f4f82c061ea29383961e4797cbedfdfd30389203d68dbc8979958dde8afdf43698e34405971b39e4804262'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TallerNT::Application.config.session_store :cookie_store, key: '_TallerNT_session'
